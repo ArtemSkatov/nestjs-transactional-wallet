1cb8e138384bc493916ae802319af83f1ea4b899
Artem Shkatov <shkatov.87@list.ru>
2026-02-26 22:27:29 +0500

    init prisma

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma/prisma.service';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly prisma: PrismaService) {}
+  async getHello() {
+    try {
+      const users = await this.prisma.user.findMany();
+      return users;
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        console.error(error.message);
+      } else {
+        console.error(error);
+      }
+      return []; // безопасный fallback
+    }
   }
 }
